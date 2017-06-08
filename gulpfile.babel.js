637cb6ad07f41ab95b915adb0732b97252389f17
Keshav <keshav@particle41.com>
2017-06-08 11:17:14 +0530

    Created serverless project structure

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,16 +3,20 @@ import babel from 'gulp-babel';
 import del from 'del';
 
 gulp.task("clear-babel-output", callback => {
-    
     del(["bin/*", "!.gitkeep"])
         .then(()=>callback())
         .catch(callback);
 });
 
-gulp.task("babel-transpile", ["clear-babel-output"], () => {
-    return gulp.src("src/*")
+gulp.task("copy-non-js", ["clear-babel-output"], () => {
+    return gulp.src("src/**/*.!(js)")
+        .pipe(gulp.dest("bin"));
+});
+
+gulp.task("babel-transpile", ["copy-non-js"], () => {
+    return gulp.src("src/**/*.js")
         .pipe(babel())
         .pipe(gulp.dest("bin"));
 });
 
-gulp.task("build", ["babel-transpile", "clear-babel-output"]);
+gulp.task("build", ["babel-transpile"]);
---
+++ src/services/my-service/index.js
@@ -0,0 +1,4 @@
+
+export const handler = (event, context, callback) => {
+
+};
