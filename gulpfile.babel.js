7e6cbf5e53b6faba2f7e489804e56e390113222b
Keshav <keshav@particle41.com>
2017-06-08 16:00:02 +0530

    refactored gulpfile to be more performant.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,21 +2,21 @@ import gulp from 'gulp';
 import babel from 'gulp-babel';
 import del from 'del';
 
-gulp.task("clear-babel-output", callback => {
+gulp.task("clear-output", callback => {
     del(["bin/*", "!.gitkeep"])
-        .then(()=>callback())
+        .then(() => callback())
         .catch(callback);
 });
 
-gulp.task("copy-non-js", ["clear-babel-output"], () => {
+gulp.task("copy-non-js", ["clear-output"], () => {
     return gulp.src("src/**/*.!(js)")
         .pipe(gulp.dest("bin"));
 });
 
-gulp.task("babel-transpile", ["copy-non-js"], () => {
+gulp.task("babel-transpile", ["clear-output"], () => {
     return gulp.src("src/**/*.js")
         .pipe(babel())
         .pipe(gulp.dest("bin"));
 });
 
-gulp.task("build", ["babel-transpile"]);
+gulp.task("build", ["babel-transpile", "copy-non-js"]);
